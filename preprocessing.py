4898edd7a1ab3ea88305c8f086ed27a6aa94fcdf
Srishti <srishti387@gmail>
2021-09-19 19:27:20 +0530

    Added support for saving & Reading intermediate files

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import texthero as hero
 import re
+from num2words import num2words
 
 
 def basic_preprocessing(series):
@@ -22,8 +23,36 @@ def basic_preprocessing(series):
 
     return series
 
-
 def custom_preprocessing(text):
-    text = re.sub(r"[^A-Za-z0-9,!.']", " ", text)
+    text = re.sub(r"[^A-Za-z0-9,.!']", " ", text)
+    text = re.sub(r"(\s)(\s)+", ' ', text)
+    return text
+
+
+def custom_preprocessing_lstm(text):
+    # convert 1 to one, etc.
+    num2words_function = lambda y: num2words(y) if y.isnumeric() else y
+    text_list = text.split(" ")
+    text = " ".join(list(map(num2words_function, text_list))).strip()
+
+    # cleaning apostrophes
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"^[A-Za-z]\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+
+    text = re.sub(r" e g ", " example ", text)
+    text = re.sub(r" eg ", " example ", text)
+    text = re.sub(r" i.e ", " that is ", text)
+
+    text = re.sub(r"\$", " dollar ", text)
+    text = re.sub(r"\€", " euro ", text)
+
+    text = re.sub(r"[^A-Za-z,.']", " ", text)
     text = re.sub(r"(\s)(\s)+", ' ', text)
     return text
---
+++ utils.py
@@ -0,0 +1,35 @@
+import joblib
+import os
+import json
+from datetime import datetime
+import keras
+
+
+def save_files(file, filename):
+    output_path = os.path.join(os.getcwd(), 'SavedFiles', str(filename + ".pkl"))
+    joblib.dump(file, output_path)
+
+
+def read_pickles(filename):
+    path = os.path.join(os.getcwd(), 'SavedFiles', str(filename))
+    file = joblib.load(path)
+    return file
+
+
+def save_lstm(model):
+    config_filename = 'config.json'
+    config = json.load(open(os.path.join(os.getcwd(), 'Data Files', str(config_filename))))
+    filename1 = config['filename1'].split(".")[0]
+    filename2 = config['filename2'].split(".")[0]
+    time_stamp = datetime.now().strftime("%d-%m-%Y_%H-%M")
+    filename = str(filename1 + "_" + filename2 + "_model_" + time_stamp)
+    path = os.path.join(os.getcwd(), 'SavedFiles', str(filename))
+    keras.models.save_model(model, path, save_format='h5')
+    print("Model Saved Successfully")
+
+
+def read_lstm(model_name):
+    path = os.path.join(os.getcwd(), 'SavedFiles', str(model_name))
+    model = keras.models.load_model(path)
+    print("Model Read Successfully")
+    return model
